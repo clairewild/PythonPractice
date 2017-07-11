73400501461fec1992afe8e6e59c164dfe5b676f
Claire Wild <claire.wild15@gmail.com>
2017-07-11 15:04:20 -0700

    Clean up code

--- leet_code.py
+++ leet_code.py
@@ -4,23 +4,21 @@ class UndirectedGraphNode:
         self.neighbors = []
 
 class Solution:
-    # @param node, an undirected graph node
-    # @return an undirected graph node
     def cloneGraph(self, node):
         new_node = UndirectedGraphNode(node.label)
         nodes_dict = {}
-        stack = []
+        queue = []
 
         nodes_dict[new_node.label] = new_node
-        stack.append(node)
+        queue.append(node)
 
-        while stack.length > 0:
-            current_node = stack.pop()
+        while queue.length > 0:
+            current_node = queue.pop(0)
 
             for neighbor in current_node.neighbors:
-                if !nodes_dict[neighbor.label]:
+                if neighbor.label not in nodes_dict:
                     nodes_dict[neighbor.label] = UndirectedGraphNode(neighbor.label)
-                    stack.append(neighbor)
+                    queue.append(neighbor)
 
                 nodes_dict[current_node.label].neighbors.append(nodes_dict[neighbor.label])
 
